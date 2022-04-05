ab00806562483b69c86c00d3e9818bbd7cd815a7
piwiit <bouillaux@hotmail.fr>
2022-04-05 22:45:53 +0200

    add rgb function

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const body = document.querySelector('body');
-const p = document.querySelector('p');
 
 const randColor = () => {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 };
 
+const hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+
 const generator = (color) => {
   body.style.background = color;
-  p.textContent = color;
+  hex.textContent = color;
+  rgb.textContent = `rgb( ${hexToRgb(color).r}, ${hexToRgb(color).g}, ${hexToRgb(color).b})`;
 };
 
 const loop = () => {
