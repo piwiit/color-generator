aacff1ee582ea7a4675ec0bb12ab1d392b848dea
piwiit <bouillaux@hotmail.fr>
2022-04-05 21:58:48 +0200

    add hex color && logical

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const body = document.querySelector('body');
+const p = document.querySelector('p');
 
 const randColor = () => {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 };
 
-const addBackground = (color) => {
+const generator = (color) => {
   body.style.background = color;
+  p.textContent = color;
 };
 
 const loop = () => {
   setTimeout(() => {
-    addBackground(randColor());
+    generator(randColor());
     loop();
   }, 3000);
 };
+
+body.addEventListener('click', () => generator(randColor()));
 
 loop();
